5a3b2f0924859249152aa63ba79aff83b59c02b0
Avinash Yadav <58301843+avi438@users.noreply.github.com>
2024-05-22 06:36:24 +0530

    Initial commit

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+
+const baseURL =
+  "https://api.weatherapi.com/v1/current.json?key=dc3481fcb8f54adbb8500402242205";
+
+export const getWeatherDataForCity = async (city) => {
+  const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
+  return await response.json();
+};
+
+export const getWeatherDataForLocation = async (lat, lon) => {
+  const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
+  return await response.json();
+};
+
